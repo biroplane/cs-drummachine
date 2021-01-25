ae97c7f0ba6c1c7ed300c2e0704c6a90457255ae
Gaetano Vallarelli <biroplane@users.noreply.github.com>
2021-01-25 17:04:50 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+  <div class="container">
+  <pad v-for="(pad,p) in pads" :key="p" :pad="pad" />
+  
+  </div>
+  </div>
+</template>
+
+<script>
+import Pad from "@/components/Pad";
+
+export default {
+  name: "App",
+  data(){
+    return {
+      pads:[
+        {trigger:"1",sound:"AverageKick.wav",label:"Kick"},
+        {trigger:"2",sound:"FunkHat.wav",label:"Hat"},
+        {trigger:"3",sound:"SmackSnare.wav",label:"Snare"},
+        {trigger:"4",sound:"AverageKick.wav",label:"Kick"},
+        {trigger:"q",sound:"FunkHat.wav",label:"Kick"},
+        {trigger:"w",sound:"SmackSnare.wav",label:"Kick"},
+        {trigger:"e",sound:"AverageKick.wav",label:"Kick"},
+        {trigger:"r",sound:"FunkHat.wav",label:"Kick"},
+        {trigger:"a",sound:"SmackSnare.wav",label:"Kick"},
+        {trigger:"s",sound:"AverageKick.wav",label:"Kick"},
+        {trigger:"d",sound:"FunkHat.wav",label:"Kick"},
+        {trigger:"f",sound:"SmackSnare.wav",label:"Kick"},
+        {trigger:"z",sound:"AverageKick.wav",label:"Kick"},
+        {trigger:"x",sound:"FunkHat.wav",label:"Kick"},
+        {trigger:"c",sound:"SmackSnare.wav",label:"Kick"},
+        {trigger:"v",sound:"SmackSnare.wav",label:"Kick"},
+      ]
+    }
+  },
+  components:{
+    Pad
+  },
+  created(){
+    window.addEventListener('keydown',(e)=>{
+      console.log("Keydown",e)
+    })
+  }
+  
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  color: #2c3e50;
+  margin-top: 60px;
+  display:flex;
+  width:100vw;
+  height:100vh;
+  justify-content:center;
+  align-items:center;
+
+}
+.container{
+  display:grid;
+  grid-template-columns: repeat(4, minmax(100px, 1fr));
+  column-gap:1rem;
+  row-gap:1rem;
+  max-width:500px;
+}
+</style>
---
+++ src/components/Pad.vue
@@ -0,0 +1,67 @@
+<template>
+  <button class="pad" :class="hit ? 'down':null" @mousedown="push(true)" @mouseup="push(false)" >
+    {{pad.label}} - ({{pad.trigger}})
+  </button>
+</template>
+<script>
+export default {
+  props:{
+    pad:{
+      type:Object,
+      default:()=>{}
+    }
+  },
+  data(){
+    return{
+      hit:false,
+      audio:''
+    }
+  },
+  mounted(){
+    this.audio=new Audio(`/sounds/${this.pad.sound}`);
+  },
+  methods:{
+    push(val){
+      console.log("CLICK",this.audio)
+      if(this.hit){
+        
+        this.audio.play()
+
+      }else{
+        this.audio.pause();
+        this.audio.currentTime = 0;
+
+      }
+    },
+   
+  }
+}
+</script>
+<style lang="scss">
+.pad{
+  width:100%;
+  aspect-ratio:1 / 1;
+  border:none;
+  text-transform:uppercase;
+  font-weight:bold;
+  color:white;
+  background-image:linear-gradient(#F00,#F6F);
+  border-radius:5px;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  cursor:pointer;
+  transition:all .02s ease-in-out;
+  &.down{
+  background-image:linear-gradient(#FF0,#06F);
+
+  }
+  &:hover{
+    box-shadow: 0px -4px 10px #F88;
+    transform:translateY(2px);
+  }
+  &:focus{
+    outline:none;
+  }
+}
+</style>
